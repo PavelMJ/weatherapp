94455f9a23ccc758537fa950264c19c384ecd66e
PavelMJ <117603528+PavelMJ@users.noreply.github.com>
2023-06-08 21:19:31 +0300

    trial icon and symbol

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,17 @@ export default function Home() {
 				<input className='search-input' type="text" placeholder='Lockation'/>
 				<button className='search-btn'>Search</button>
 			</div>
-			<div></div>
+			<div className='city'>
+				<div className='wether-info'>
+					<div className='data'>
+						<div className='temperature'>27 С	&deg;</div>
+						<img src="img/cloud_sun_sunny_weather_icon.svg" alt="" />
+						<div className='info'></div>
+					</div>
+					<div className='name-of-city'></div>
+				</div>
+				<button className='add-to-favorite'></button>
+			</div>
 			<div></div>
 
 
