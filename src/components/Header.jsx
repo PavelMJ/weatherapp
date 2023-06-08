0eb2769da4ad8031c179728a2c18cd90dfa70621
PavelMJ <117603528+PavelMJ@users.noreply.github.com>
2023-06-08 21:09:26 +0300

    started design

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+export default function Header() {
+	return (
+		<div className='App-header' >
+			<h2>Whethers</h2>
+			<div className='nav-btns'>
+				<Link to='/'><button className='header-btn'>Home</button></Link>
+				<Link to='/favorites'><button className='header-btn'>Favorites</button></Link>
+			</div>
+		</div>
+	)
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home() {
+	return (
+		<div className='Home'>
+			<div className='input-panel'>
+				<input className='search-input' type="text" placeholder='Lockation'/>
+				<button className='search-btn'>Search</button>
+			</div>
+			<div></div>
+			<div></div>
+
+
+		</div>
+	)
+}
